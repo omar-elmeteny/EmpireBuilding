7f55446657300284fa2cc018833f73724238cd3a
omar.elmeteny <omar.elmeteny@gmail.com>
2021-06-22 23:46:39 +0200

    Added game map

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -2,6 +2,7 @@ package view;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.io.IOException;
 
 import engine.Game;
 
@@ -11,13 +12,14 @@ public class GameView extends JPanel{
     private MapView mapView;
     private SideView sideView;
 
-    public GameView(Game game) {
+    public GameView(Game game) throws IOException{
         super();
         setLayout(new BorderLayout());
         summaryView = new SummaryView(game);
         this.add(summaryView, BorderLayout.NORTH);
 
-
+        mapView = new MapView();
+        this.add(mapView, BorderLayout.CENTER);
     }
 
 }
--- src/view/MapView.java
+++ src/view/MapView.java
@@ -1,7 +1,40 @@
 package view;
 
 import javax.swing.JPanel;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.Graphics;
 
 public class MapView extends JPanel{
     
+    private BufferedImage image;
+
+    public MapView() throws IOException{
+        image = ImageIO.read(new File("map.png"));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        int newWidth;
+        int newHeight;
+
+        if (panelWidth * imageHeight < panelHeight * imageWidth) {
+            newWidth = panelWidth;
+            newHeight = panelWidth * imageHeight / imageWidth;
+        } else {
+            newHeight = panelHeight;
+            newWidth = panelHeight * imageWidth / imageHeight;
+        }
+        g.drawImage(image, 0, 0, newWidth, newHeight, this);            
+    }
+
 }
